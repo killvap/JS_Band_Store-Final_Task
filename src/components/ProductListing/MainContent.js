f45ccf18130a63c892231ca2700884059dfc2a8c
Pavlo Bezuglyi <killvaps@gmail.com>
2021-08-27 21:56:23 +0300

    fixed displaying image(after changes in package.json)

--- src/components/ProductListing/MainContent.js
+++ src/components/ProductListing/MainContent.js
@@ -19,13 +19,14 @@ const MainContent = (props) => {
 		);
 	}
 
+
 	return (
 		<div className="main_content">
 			{noResultMessage}
 			{testData.map((item) => (
 				<div className="card" key={(item = Math.random().toString())}>
 					<div className="card_img">
-						<img src={item.imageLink} />
+						<img src={`${process.env.PUBLIC_URL}/${item.imageLink}`} alt={item.title} />
 					</div>
 					<div className="card_header">
 						<h2>{item.product_name}</h2>
