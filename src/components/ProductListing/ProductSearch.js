4501e4a80e91d82e38f95391ff6ade9aad635d82
Pavlo Bezuglyi <killvaps@gmail.com>
2021-08-27 21:36:59 +0300

    minor fixes

--- src/components/ProductListing/ProductSearch.js
+++ src/components/ProductListing/ProductSearch.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import SearchIcon from "@material-ui/icons/Search";
+import React, { useState } from "react";
+
 import "./ProductSearch.css";
 
 const ProductSearch = (props) => {
