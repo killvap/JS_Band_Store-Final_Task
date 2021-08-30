c4e3c7a3d61f15cf0264a7a68b630eacf29c2a4f
Pavlo Bezuglyi <killvaps@gmail.com>
2021-08-30 14:43:43 +0300

    Changed styles, added new text fields, changed btn name for card element

--- src/components/ProductListing/ProductList/ProductList.js
+++ src/components/ProductListing/ProductList/ProductList.js
@@ -2,20 +2,25 @@ import React from "react";
 
 const ProductList = (props) => {
 	return (
-		<div className="main_content">		
+		<div className="main_content">
 			{props.products.map((item) => (
 				<div className="card" key={(item = Math.random().toString())}>
 					<div className="card_img">
-						<img src={`${process.env.PUBLIC_URL}/${item.imageLink}`} alt={item.title} />
+						<img
+							src={`${process.env.PUBLIC_URL}/${item.imageLink}`}
+							alt={item.title}
+						/>
 					</div>
 					<div className="card_header">
-						<h2>{item.product_name}</h2>
-						<p>{item.description}</p>
-						<p className="price">
+						<span>{item.title}</span>
+						<p>{item.author}</p>
+					</div>
+					<div className="card_body">
+						<div className="price">
 							${item.price}
 							<span>{item.currency}</span>
-						</p>
-						<div className="btn">Add to cart</div>
+						</div>
+						<div className="btn">VIEW</div>
 					</div>
 				</div>
 			))}
